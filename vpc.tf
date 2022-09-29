399015c35f0d9a68736907987d743d6909a3856a
Kim Taekyung <seaking7@gmail.com>
2022-09-29 20:50:02 +0900

    vpc 이름 수정

--- vpc.tf
+++ vpc.tf
@@ -33,7 +33,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = "terraform-igw"
   }
 }
 
@@ -83,4 +83,10 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "route_table_association_private" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id              = aws_route_table.private.id
+  destination_cidr_block      = "0.0.0.0/0"
+  nat_gateway_id              = aws_nat_gateway.nat_gateway.id
 }
