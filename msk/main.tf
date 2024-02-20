04cc109cdc2c0b7de2b3bb80a9919b9ab323c3cc
김태경/광고플랫폼개발팀 <taekyung@lguplus.co.kr>
2024-02-20 17:44:31 +0900

    기존 VPC이용해서 MSK 생성되도록 변경

--- msk/main.tf
+++ msk/main.tf
@@ -29,9 +29,8 @@ resource "aws_msk_cluster" "kafka" {
         volume_size = 100  # default 1000
       }
     }
-    client_subnets = [aws_subnet.private_subnet[0].id,
-      aws_subnet.private_subnet[1].id]
-    security_groups = [aws_security_group.kafka.id]
+    client_subnets = [local.subnet_ids[0], local.subnet_ids[1]]
+    security_groups = local.broker_node_security_groups
   }
   encryption_info {
     encryption_in_transit {
@@ -67,142 +66,142 @@ resource "aws_msk_cluster" "kafka" {
 # General
 ################################################################################
 
-resource "aws_vpc" "default" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-}
+# resource "aws_vpc" "default" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+# }
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-}
+# resource "aws_internet_gateway" "default" {
+#   vpc_id = aws_vpc.default.id
+# }
 
-resource "aws_eip" "default" {
-  depends_on = [aws_internet_gateway.default]
-  domain     = "vpc"
-}
+# resource "aws_eip" "default" {
+#   depends_on = [aws_internet_gateway.default]
+#   domain     = "vpc"
+# }
 
-resource "aws_route" "default" {
-  route_table_id         = aws_vpc.default.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.default.id
-}
+# resource "aws_route" "default" {
+#   route_table_id         = aws_vpc.default.main_route_table_id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.default.id
+# }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.default.id
-}
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.default.id
+# }
 
-resource "aws_route_table_association" "private_subnet_association" {
-  count          = length(data.aws_availability_zones.available.names)
-  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id = aws_route_table.private_route_table.id
-}
+# resource "aws_route_table_association" "private_subnet_association" {
+#   count          = length(data.aws_availability_zones.available.names)
+#   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+#   route_table_id = aws_route_table.private_route_table.id
+# }
 
 ################################################################################
 # Subnets
 ################################################################################
 
-resource "aws_subnet" "private_subnet" {
-  count                   = length(var.private_cidr_blocks)
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = element(var.private_cidr_blocks, count.index)
-  map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-}
+# resource "aws_subnet" "private_subnet" {
+#   count                   = length(var.private_cidr_blocks)
+#   vpc_id                  = aws_vpc.default.id
+#   cidr_block              = element(var.private_cidr_blocks, count.index)
+#   map_public_ip_on_launch = false
+#   availability_zone       = data.aws_availability_zones.available.names[count.index]
+# }
 
-resource "aws_subnet" "bastion_host_subnet" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = var.cidr_blocks_bastion_host[0]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
-}
+# resource "aws_subnet" "bastion_host_subnet" {
+#   vpc_id                  = aws_vpc.default.id
+#   cidr_block              = var.cidr_blocks_bastion_host[0]
+#   map_public_ip_on_launch = true
+#   availability_zone       = data.aws_availability_zones.available.names[0]
+# }
 
 ################################################################################
 # Security groups
 ################################################################################
 
-resource "aws_security_group" "kafka" {
-  name   = "${var.global_prefix}-kafka"
-  vpc_id = aws_vpc.default.id
-  description = "msk_test_security_group"
-  ingress {
-    from_port   = 0
-    to_port     = 9092
-    protocol    = "TCP"
-    cidr_blocks = var.private_cidr_blocks
-  }
-  ingress {
-    from_port   = 0
-    to_port     = 9092
-    protocol    = "TCP"
-    cidr_blocks = var.cidr_blocks_bastion_host
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "kafka" {
+#   name   = "${var.global_prefix}-kafka"
+#   vpc_id = local.vpc
+#   description = "msk_test_security_group"
+#   ingress {
+#     from_port   = 0
+#     to_port     = 9092
+#     protocol    = "TCP"
+#     cidr_blocks = var.private_cidr_blocks
+#   }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 9092
+#     protocol    = "TCP"
+#     cidr_blocks = var.cidr_blocks_bastion_host
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_security_group" "bastion_host" {
-  name   = "${var.global_prefix}-bastion-host"
-  vpc_id = aws_vpc.default.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_security_group" "bastion_host" {
+#   name   = "${var.global_prefix}-bastion-host"
+#   vpc_id = aws_vpc.default.id
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "private_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "aws_key_pair" "private_key" {
-  key_name   = var.global_prefix
-  public_key = tls_private_key.private_key.public_key_openssh
-}
+# resource "aws_key_pair" "private_key" {
+#   key_name   = var.global_prefix
+#   public_key = tls_private_key.private_key.public_key_openssh
+# }
 
-resource "local_file" "private_key" {
-  content  = tls_private_key.private_key.private_key_pem
-  filename = "cert.pem"
-}
+# resource "local_file" "private_key" {
+#   content  = tls_private_key.private_key.private_key_pem
+#   filename = "cert.pem"
+# }
 
-resource "null_resource" "private_key_permissions" {
-  depends_on = [local_file.private_key]
-  provisioner "local-exec" {
-    command     = "chmod 600 cert.pem"
-    interpreter = ["bash", "-c"]
-    on_failure  = continue
-  }
-}
+# resource "null_resource" "private_key_permissions" {
+#   depends_on = [local_file.private_key]
+#   provisioner "local-exec" {
+#     command     = "chmod 600 cert.pem"
+#     interpreter = ["bash", "-c"]
+#     on_failure  = continue
+#   }
+# }
 
 ################################################################################
 # Client Machine (EC2)
 ################################################################################
 
-resource "aws_instance" "bastion_host" {
-  depends_on             = [aws_msk_cluster.kafka]
-  ami                    = data.aws_ami.amazon_linux_2023.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.private_key.key_name
-  subnet_id              = aws_subnet.bastion_host_subnet.id
-  vpc_security_group_ids = [aws_security_group.bastion_host.id]
-  user_data = templatefile("bastion.tftpl", {
-    bootstrap_server_1 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[0]
-    bootstrap_server_2 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[1]
-    bootstrap_server_3 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[2]
-  })
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 100
-  }
-}
+# resource "aws_instance" "bastion_host" {
+#   depends_on             = [aws_msk_cluster.kafka]
+#   ami                    = data.aws_ami.amazon_linux_2023.id
+#   instance_type          = "t2.micro"
+#   key_name               = aws_key_pair.private_key.key_name
+#   subnet_id              = aws_subnet.bastion_host_subnet.id
+#   vpc_security_group_ids = [aws_security_group.bastion_host.id]
+#   user_data = templatefile("bastion.tftpl", {
+#     bootstrap_server_1 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[0]
+#     bootstrap_server_2 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[1]
+#     bootstrap_server_3 = split(",", aws_msk_cluster.kafka.bootstrap_brokers)[2]
+#   })
+#   root_block_device {
+#     volume_type = "gp2"
+#     volume_size = 100
+#   }
+# }
--- msk/variables.tf
+++ msk/variables.tf
@@ -10,24 +10,25 @@ variable "global_prefix" {
   default = "ssp-msk"
 }
 
-variable "private_cidr_blocks" {
-  type = list(string)
-  default = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-  ]
-}
+# variable "private_cidr_blocks" {
+#   type = list(string)
+#   default = [
+#     "10.0.1.0/24",
+#     "10.0.2.0/24",
+#     "10.0.3.0/24",
+#   ]
+# }
 
-variable "cidr_blocks_bastion_host" {
-  type = list(string)
-  default = ["10.0.4.0/24"]
-}
+# variable "cidr_blocks_bastion_host" {
+#   type = list(string)
+#   default = ["10.0.4.0/24"]
+# }
 
 
 
 locals {
   name = "ssp-test2"
+  vpc = "vpc-03e87995936924eb2"
   subnet_ids      = ["subnet-065400bba8c35c98b","subnet-05e1bf16b38805205"]
   broker_node_security_groups = ["sg-0d98cab2e93226e7e"]
 }
