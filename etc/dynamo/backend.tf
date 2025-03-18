c5eba5385fc996cf0284f53df85e68f0e669f307
김태경/모바일서비스BE개발팀 <taekyung@lguplus.co.kr>
2025-03-18 15:56:34 +0900

    eks 생성 등

--- etc/dynamo/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-      bucket         = "seaking7-tfstate" # s3 bucket 이름
-      key            = "aws_workspace/etc/dynamo/terraform.tfstate" # s3 내에서 저장되는 경로
-      region         = "ap-northeast-2"  
-      encrypt        = true
-      dynamodb_table = "terraform-lock"
-    }
-}
--- etc/storage/s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "s3" {
-        bucket = "taekyung-7113"
-}
